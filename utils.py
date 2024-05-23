55af0f088f5ae70c902b4fd1b05477043dc218be
samchan2022 <samchan.goo.123@gmail.com>
2024-05-23 22:31:44 +0100

    feat: add utils

---
+++ utils.py
@@ -0,0 +1,59 @@
+import openai
+import numpy as np
+from numpy.linalg import norm
+
+def get_embeddings(texts):
+    """
+    Get embeddings for a list of texts using OpenAI's embedding model.
+
+    Parameters:
+        texts (list of str): List of texts to embed.
+
+    Returns:
+        list of list of float: List of embeddings for the input texts.
+    """
+    response = openai.Embedding.create(
+        input=texts,
+        model="text-embedding-ada-002"  # Specify the embedding model
+    )
+    embeddings = [result['embedding'] for result in response['data']]
+    return embeddings
+
+def cosine_similarity(vec1, vec2):
+    """
+    Calculate the cosine similarity between two vectors.
+
+    Parameters:
+        vec1 (list of float): First vector.
+        vec2 (list of float): Second vector.
+
+    Returns:
+        float: Cosine similarity between the vectors.
+    """
+    return np.dot(vec1, vec2) / (norm(vec1) * norm(vec2))
+
+def recommend(input_text, texts, embeddings, top_k=1):
+    """
+    Recommend texts similar to the input text.
+
+    Parameters:
+        input_text (str): The text for which to find recommendations.
+        texts (list of str): List of original texts.
+        embeddings (list of list of float): List of embeddings corresponding to the texts.
+        top_k (int): Number of top similar texts to return.
+
+    Returns:
+        list of str: List of recommended texts.
+    """
+    # Get embedding for the input text
+    input_embedding = get_embeddings([input_text])[0]
+
+    # Calculate similarities
+    similarities = [cosine_similarity(input_embedding, emb) for emb in embeddings]
+
+    # Get the indices of the top_k similar texts
+    top_k_indices = np.argsort(similarities)[-top_k:][::-1]
+
+    # Return the most similar texts
+    return [texts[i] for i in top_k_indices]
+
